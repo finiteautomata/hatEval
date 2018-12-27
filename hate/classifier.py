7772b4472512ae2ee649c00f693d6f1e571737bf
Franco M. Luque <francolq@famaf.unc.edu.ar>
2018-12-27 18:36:52 -0300

    Hate classifier with configurable language.

--- hate/classifier.py
+++ hate/classifier.py
@@ -1,5 +1,15 @@
 from sentiment.classifier import SentimentClassifier
+from hate.tokenizer import Tokenizer
 
 
 class HateClassifier(SentimentClassifier):
-    pass
+
+    def __init__(self, lang='en', **kwargs):
+        self._lang = lang
+        super().__init__(**kwargs)
+
+    def build_bow_tokenizer(self):
+        return Tokenizer(lang=self._lang, rdup=True)
+
+    def build_emb_tokenizer(self):
+        return Tokenizer(lang=self._lang, lem=False)
