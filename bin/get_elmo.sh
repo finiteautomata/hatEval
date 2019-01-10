421e68b98b46e2dfcb4417877baf174628d53db3
Juan Manuel Pérez <jmperez.85@gmail.com>
2019-01-10 15:20:46 -0300

    Agrego modelo con ElMO

--- bin/get_elmo.sh
+++ bin/get_elmo.sh
@@ -5,8 +5,10 @@ mkdir -p es && cd es
 wget -nc http://vectors.nlpl.eu/repository/11/145.zip
 unzip 145.zip
 rm 145.zip
+cd ..
 # Get English
 mkdir -p en && cd en
 wget -nc http://vectors.nlpl.eu/repository/11/144.zip
 unzip 144.zip
 rm 144.zip
+cd ..
