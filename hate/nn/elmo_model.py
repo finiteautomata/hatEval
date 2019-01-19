aa0a269a4188ca1105a66fe1dff9d2e7ab642673
jmperez <jmperez.85@gmail.com>
2019-01-19 18:19:54 -0300

    Modelo ELMO terminado.

--- hate/nn/elmo_model.py
+++ hate/nn/elmo_model.py
@@ -1,6 +1,5 @@
 from .preprocessing import Tokenizer
-from keras.preprocessing.text import Tokenizer as KerasTokenizer
-from keras.preprocessing.sequence import pad_sequences
+import numpy as np
 from keras.layers import (
     Input, Embedding, Dense, Dropout, Bidirectional, LSTM,
     MaxPooling1D, Conv1D,
@@ -10,11 +9,12 @@ from elmoformanylangs import Embedder
 
 
 class ElmoModel(keras.Model):
-    def __init__(self, max_len, path_to_elmo_model, tokenize_args={},
+    def __init__(self, max_len, embedder, tokenize_args={},
                  recursive_class=LSTM, lstm_units=128, dropout=[0.75, 0.50],
                  dense_units=128):
 
         self._max_len = max_len
+        self._embedder = embedder
         self._tokenizer = Tokenizer(**tokenize_args)
         self._elmo_dim = 1024
         # Build the graph
@@ -24,44 +24,46 @@ class ElmoModel(keras.Model):
         y = Dense(dense_units, activation='relu', name='dense_elmo')(y)
         output = Dense(1, activation='sigmoid', name='output')(y)
 
-        self._embedder = Embedder(path_to_elmo_model)
 
         super().__init__(inputs=[input_elmo], outputs=[output])
 
-    def _preprocess(self, X):
-        tokens = map(self._tokenizer.tokenize, X)
-        instances = [" ".join(seq_tokens) for seq_tokens in tokens]
+    def preprocess_fit(self, X):
+        return
 
-        return instances
+    def preprocess_transform(self, X):
+        list_of_tokens = [self._tokenizer.tokenize(t) for t in X]
+
+        padded_tokens = []
+        for tokens in list_of_tokens:
+            if len(tokens) >= self._max_len:
+                tokens = tokens[:self._max_len]
+            else:
+                tokens = tokens + [''] * (self._max_len - len(tokens))
+
+            padded_tokens.append(tokens)
+
+        elmo_embeddings = self._embedder.sents2elmo(padded_tokens)
+        return np.array(elmo_embeddings)
 
     def fit(self, X, y, validation_data=None, **kwargs):
-        text_train = self._preprocess(X)
+        self.preprocess_fit(X)
 
-        self._char_tokenizer.fit_on_texts(text_train)
-
-        X_train = self._char_tokenizer.texts_to_sequences(text_train)
-        X_train = pad_sequences(X_train, self._max_charlen)
+        X_train = self.preprocess_transform(X)
 
         val_data = None
         if validation_data:
-            text_val = self._preprocess(validation_data[0])
-            X_val = self._char_tokenizer.texts_to_sequences(text_val)
-            X_val = pad_sequences(X_val, self._max_charlen)
+            X_val = self.preprocess_transform(validation_data[0])
             y_val = validation_data[1]
             val_data = (X_val, y_val)
 
         super().fit(X_train, y, validation_data=val_data, **kwargs)
 
     def evaluate(self, X, y=None, **kwargs):
-        X = self._preprocess(X)
-        X = self._char_tokenizer.texts_to_sequences(X)
-        X = pad_sequences(X, self._max_charlen)
+        X = self.preprocess_transform(X)
 
         return super().evaluate(X, y, **kwargs)
 
     def predict(self, X, **kwargs):
-        X = self._preprocess(X)
-        X = self._char_tokenizer.texts_to_sequences(X)
-        X = pad_sequences(X, self._max_charlen)
+        X = self.preprocess_transform(X)
 
         return super().predict(X, **kwargs)
