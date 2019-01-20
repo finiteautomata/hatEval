9eec469483a9f640fcf6e386a0753af23a71b6d0
jmperez <jmperez.85@gmail.com>
2019-01-19 22:35:12 -0300

    Actualizo scripts para obtener datos de test.

--- bin/get_data.sh
+++ bin/get_data.sh
@@ -30,4 +30,16 @@ mv $DATA_DIR/public_development_en $DATA_DIR/en
 rm $SPANISH_A_ZIP
 rm $ENGLISH_A_ZIP
 
-#wget https://competitions.codalab.org/my/datasets/download/70f24fdb-2df2-4a9c-9bec-2ec00e2f5554 -O "$DATA_DIR/dev_english_a.zip"
+# Get Evaluation Data
+
+SPANISH_EVALUATION_A="https://competitions.codalab.org/my/datasets/download/4d4a0bfb-7568-4a09-8e68-45c39dc2e7c6"
+SPANISH_A_TEST_FILE="$DATA_DIR/es/evaluation_spanish_a.zip"
+wget $SPANISH_EVALUATION_A -O $SPANISH_A_TEST_FILE
+7z e $SPANISH_A_TEST_FILE -o"$DATA_DIR/es" -p$SPANISH_PASS
+rm $SPANISH_A_TEST_FILE
+
+ENGLISH_EVALUATION_A="https://competitions.codalab.org/my/datasets/download/c50d8652-0fcb-4712-8a9f-69238222e313"
+ENGLISH_A_TEST_FILE="$DATA_DIR/en/evaluation_english_a.zip"
+wget $ENGLISH_EVALUATION_A -O $ENGLISH_A_TEST_FILE
+7z e $ENGLISH_A_TEST_FILE -o"$DATA_DIR/en" -p$ENGLISH_PASS
+rm $ENGLISH_A_TEST_FILE
